b5095f7fb8f6f17b7b35fbe2885de2c3eeda1feb
renjie <1103752053@qq.com>
2020-11-03 16:30:28 +0800

    修正最近状态

--- test/post/start.js
+++
@@ -1,9 +0,0 @@
-var ser = require('./server');//服务模块
-var handler = require('./handle');//处理模块
-var router = require('./router');//路由模块
- 
-var handle = {};
-handle['/'] = handler.index;
-handle['/test'] = handler.test;
-handle['/requide'] = handler.requide;//处理模块的函数封装到对象数组里
-ser.serve(router.router,handle);
